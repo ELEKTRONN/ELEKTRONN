b7aa2805420ade1ecb8c5fc9e922a0cd66227650
mdraw <mdraw.gh@gmail.com>
2016-01-13 19:50:16 +0100

    Improve distribution, make it ready for PyPi
    Fix Cython dependency cycle.
    Installation of ELEKTRONN via pip2 install without any pre-installed
    packages works now. You only need python==2.7 and setuptools>=18.0,
    the rest is set up automatically.
    
    Declare scikit-learn as an optional dependency.
    
    Some minor fixes

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-import os
-from setuptools import setup, find_packages, Extension
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    raise ImportError(
-        'Cython not found. Please use Anaconda\nor install the required modules '
-        'via "pip install -r requirements.txt.')
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+import os
+import setuptools
+from setuptools import setup, find_packages, Extension
+from pkg_resources import parse_version
+
+# Setuptools >=18.0 is needed for Cython to work correctly.
+if parse_version(setuptools.__version__) < parse_version('18.0'):
+    raise ImportError(
+        'Your installed Setuptools version is too old.\n'
+        '  Please upgrade it to at least 18.0, e.g. by running\n'
+        '  "pip2 install --upgrade setuptools".')
 
 
 def read(fname):
+    """
+    Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
@@ -27,11 +32,13 @@ ext_modules = [
 
 setup(
     name="ELEKTRONN",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     scripts=['elektronn/scripts/TrainCNN.py',
-             'elektronn/scripts/Profiling.py', ],
-    ext_modules=cythonize(ext_modules),
+             'elektronn/scripts/Profiling.py',
+    ],
+    ext_modules=ext_modules,
+    setup_requires=['cython'],
     install_requires=[
         'numpy',
         'scipy',
@@ -40,19 +47,19 @@ setup(
         'theano',
         'cython',
     ],
-    dependency_links=['git+http://github.com/Theano/Theano.git#egg=Theano'],
+    extras_require={'Cross-validation': ['scikit-learn']},
     author="Marius Killinger, Gregor Urban",
     author_email="Marius.Killinger@mailbox.org",
-    description=
-    ("ELEKTRONN a is highly configurable toolkit for training 3d/2d CNNs and general Neural Networks"
-     ),
+    description=("ELEKTRONN a is highly configurable toolkit for training 3d/2d CNNs and general Neural Networks"),
     long_description=read('README.rst'),
     license="GPL",
     keywords="cnn theano neural network machine learning classification",
     url="http://www.elektronn.org/",
     classifiers=[
+        'Programming Language :: Python :: 2.7',
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Information Analysis",
-    ], )
+    ],
+)
